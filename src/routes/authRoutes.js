420ec03ab0a09ed50f433cd15158cdad5a1ef069
Omar-mk123 <139969084+Omar-mk123@users.noreply.github.com>
2026-08-21 15:40:52 +0300

    Create authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const {
+  register,
+  login,
+} = require("../controllers/authController");
+
+const protect = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/me", protect, (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+      role: req.user.role,
+    },
+  });
+});
+
+module.exports = router;
